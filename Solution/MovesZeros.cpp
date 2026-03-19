90a0353fe9a88393e7f4b998690a995c5f9735d6
abhishek kumar mandal <abhishekkrmandal28@gmail.com>
2026-03-19 22:44:15 +0530

    Added Move Zeroes - Two Pointer Approach

---
+++ Solution/MovesZeros.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+
+        int n = nums.size();
+        int j = -1;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == 0){
+                j = i;
+                break; }}
+        if(j == -1) return;
+        for(int i = j + 1; i < n; i++){
+            if(nums[i] != 0){
+                swap(nums[i], nums[j]);
+                j++;
+            }
+        }
+    }
+};
+
+int main() {
+  vector<int> nums ={0,1,0,3,12};
+  Solution s1;
+  s1.moveZeroes(nums);
+  for(int num : nums) {
+    cout << num << " ";
+  }
+  return 0;
+}
